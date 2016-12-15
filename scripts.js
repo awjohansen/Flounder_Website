ea7263952ff17213867db551125b3f04d3379a14
Adam Johansen <Goblin@Adams-MacBook-Pro.local>
2016-12-14 19:01:55 -0500

    here you go

--- scripts.js
+++ scripts.js
@@ -1,6 +1,20 @@
-$(document).ready(function(){
 
 
+var token = '4204090804.2626588.af19ceeca0e84535a8f8d976cb35e129',
+      num_photos = 24;
 
-
-});
+  $.ajax({
+      url: 'https://api.instagram.com/v1/users/self/media/recent',
+      dataType: 'jsonp',
+      type: 'GET',
+      data: {access_token: token, count: num_photos},
+      success: function(data){
+          console.log(data);
+          for( x in data.data ){
+              $('ul').append('<li><img src="'+data.data[x].images.low_resolution.url+'"></li>');
+          }
+      },
+      error: function(data){
+          console.log(data);
+      }
+    });
